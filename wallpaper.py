af628b0685f75e1d138002238356777e223ad204
Eric Grube <grubeeric79@gmail.com>
2015-03-26 15:33:29 -0400

    Initial Commit

---
+++ wallpaper.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python2.7
+#change the wallpaper every day to the top post of the day on /r/wallpapers
+#Eric Grube
+
+import re, praw, requests, os, glob, sys, ctypes
+from bs4 import BeautifulSoup
+
+MIN_SCORE = 100 # the default minimum score before it is downloaded
+
+if len(sys.argv) < 2:
+    # no command line options sent:
+    #print('Usage:')
+    #print('  python %s subreddit [minimum score]' % (sys.argv[0]))
+	targetSubreddit = "wallpapers"
+    #sys.exit()
+elif len(sys.argv) >= 2:
+    # the subreddit was specified:
+    #targetSubreddit = sys.argv[1]
+    targetSubreddit = "wallpapers"
+    if len(sys.argv) >= 3:
+        # the desired minimum score was also specified:
+        MIN_SCORE = int(sys.argv[2])
+
+
+imgurUrlPattern = re.compile(r'(http://i.imgur.com/(.*))(\?.*)?')
+
+
+def downloadImage(imageUrl, localFileName):
+    response = requests.get(imageUrl)
+    if response.status_code == 200:
+        print localFileName
+        print('Downloading %s...' % (localFileName))
+        with open(localFileName, 'wb') as fo:
+            for chunk in response.iter_content(4096):
+                fo.write(chunk)
+
+def setImage(localFileName):
+    print "Hi There I am executing!"
+    filePath = os.path.abspath(localFileName)
+    print filePath
+    SPI_SETDESKWALLPAPER = 20;
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, filePath, 0)
+    print "And I did it!"
+
+# Connect to reddit and download the subreddit front page
+r = praw.Reddit(user_agent='This guy goes to a subreddit and grabs the top post. Designed to go to /r/wallpapers and get the top pic for a wallpaper of the day') 
+submissions = r.get_subreddit(targetSubreddit).get_hot(limit=1)
+# Or use one of these functions:
+#                                       .get_top_from_year(limit=25)
+#                                       .get_top_from_month(limit=25)
+#                                       .get_top_from_week(limit=25)
+#                                       .get_top_from_day(limit=25)
+#                                       .get_top_from_hour(limit=25)
+#                                       .get_top_from_all(limit=25)
+
+# Process all the submissions from the front page
+for submission in submissions:
+    # Check for all the cases where we will skip a submission:
+    if "imgur.com/" not in submission.url:
+        continue # skip non-imgur submissions
+    if submission.score < MIN_SCORE:
+        continue # skip submissions that haven't even reached 100 (thought this should be rare if we're collecting the "hot" submission)
+    if len(glob.glob('reddit_%s_%s_*' % (targetSubreddit, submission.id))) > 0:
+        continue # we've already downloaded files for this reddit submission
+
+    if 'http://imgur.com/a/' in submission.url:
+        # This is an album submission.
+        albumId = submission.url[len('http://imgur.com/a/'):]
+        htmlSource = requests.get(submission.url).text
+
+        soup = BeautifulSoup(htmlSource)
+        matches = soup.select('.album-view-image-link a')
+        for match in matches:
+            imageUrl = match['href']
+            if '?' in imageUrl:
+                imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+            else:
+                imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+            localFileName = 'reddit_%s_%s_album_%s_imgur_%s' % (targetSubreddit, submission.id, albumId, imageFile)
+            downloadImage('http:' + match['href'], localFileName)
+            setImage(localFileName)
+
+    elif 'http://i.imgur.com/' in submission.url:
+        # The URL is a direct link to the image.
+        mo = imgurUrlPattern.search(submission.url) # using regex here instead of BeautifulSoup because we are pasing a url, not html
+
+        imgurFilename = mo.group(2)
+        if '?' in imgurFilename:
+            # The regex doesn't catch a "?" at the end of the filename, so we remove it here.
+            imgurFilename = imgurFilename[:imgurFilename.find('?')]
+
+        localFileName = 'reddit_%s_%s_album_None_imgur_%s' % (targetSubreddit, submission.id, imgurFilename)
+        downloadImage(submission.url, localFileName)
+        setImage(localFileName)
+
+    elif 'http://imgur.com/' in submission.url:
+        # This is an Imgur page with a single image.
+        htmlSource = requests.get(submission.url).text # download the image's page
+        soup = BeautifulSoup(htmlSource)
+        imageUrl = soup.select('.image a')[0]['href']
+        if imageUrl.startswith('//'):
+            # if no schema is supplied in the url, prepend 'http:' to it
+            imageUrl = 'http:' + imageUrl
+        imageId = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('.')]
+
+        if '?' in imageUrl:
+            imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+        else:
+            imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+
+        localFileName = 'reddit_%s_%s_album_None_imgur_%s' % (targetSubreddit, submission.id, imageFile)
+        downloadImage(imageUrl, localFileName)
+        setImage(localFileName)
